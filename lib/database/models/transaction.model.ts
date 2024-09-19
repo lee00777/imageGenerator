bf52ffb11cb9f57ec417f0cadc2d81ef5962437f
Gyuyoung <gyuyoung.lee@statcan.gc.ca>
2024-09-19 12:00:30 +0900

    Added model schemas for user,image and transaction

---
+++ lib/database/models/transaction.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model, models } from "mongoose";
+
+// user와 image를 연결해주는 역할. 즉, user가 image 생성할때마다 credits차감하고 트랙킹하는 용도
+
+const TransactionSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  stripeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  plan: {
+    type: String,
+  },
+  credits: {
+    type: Number,
+  },
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Transaction = models?.Transaction || model("Transaction", TransactionSchema);
+
+export default Transaction;
