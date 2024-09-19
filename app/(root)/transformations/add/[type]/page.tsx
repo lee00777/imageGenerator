7f2b6275e4d385bbb6e984bb394d04be5e158db8
Gyuyoung <gyuyoung.lee@statcan.gc.ca>
2024-09-19 15:54:32 +0900

    Added custom field component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,22 +1,33 @@
 import Header from '@/components/shared/Header';
 import TransformationForm from '@/components/shared/TransformationForm';
 import { transformationTypes } from '@/constants';
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
 // export default function AddTransformationPage(props) {
-export default function AddTransformationPage({params:{type}}:SearchParamProps) {
+export default async function AddTransformationPage({params:{type}}:SearchParamProps) {
   // 원래는 props는 이렇게 리턴 => type: { params: { type: 'restore' }, searchParams: {} }
-
+  const { userId } = auth(); // clerk에서 제공하는 함수사용해서 사용자 id 받기
   const transformation = transformationTypes[type];
+  if(!userId) redirect('/sign-in')
+  const user = await getUserById(userId);// mongodb가서 사용자 정보 가져오기
+
   return (
     <>
       <Header 
         title={transformation.title}
         subtitle={transformation.subTitle}
-      >
-      </Header>
-      <TransformationForm />
+      />
+      <section className="mt-10">
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
-
   );
 }
 
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -3,24 +3,21 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
 import { defaultValues } from "@/constants"
+import { Form } from "../ui/form";
+import { CustomField } from "./CustomField"
+import { Input } from "../ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 
-
-// form validation
-const formSchema = z.object({
+// form validation schema 작성
+export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
   color: z.string().optional(),
@@ -37,6 +34,7 @@ export default function TransformationForm({action, data = null}:TransformationF
     prompt: data?.prompt,
     publicId: data?.publicId,
   } : defaultValues
+
  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -45,31 +43,39 @@ export default function TransformationForm({action, data = null}:TransformationF
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values)
   }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+        <CustomField 
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          name="title"
+          formLabel="Image Title"
+          className="w-full"
+          render={({field}) =>
+            <Input {...field} className="input-field" />
+          }
         />
-        <Button type="submit">Submit</Button>
+
+        {/* { type === 'fill' && (
+          <CustomField 
+            render={({field})=>(
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        )} */}
+
       </form>
     </Form>
   );
